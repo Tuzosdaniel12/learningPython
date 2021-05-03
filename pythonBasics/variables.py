f671d28ce5fefd2689dd554b172f035d44c90060
daniel <danielsoledad@gmail.com>
2021-05-02 21:32:49 -0700

    one file got ignore

--- pythonBasics/variables.py
+++ pythonBasics/variables.py
@@ -4,5 +4,22 @@ print("Hello,", first_name)
 
 print(first_name, "is learning Python")
 
+7 // 23 #this will return the whole number
 
+7 / 23 #this will return the decimal
 
+quote = "random quote to test build in functions"
+
+print(quote.upper())
+
+print(quote.lower())
+
+print(quote.title())
+
+print(quote.capitalize())
+
+subject_template = "Thanks for learning {} with us {}!"
+
+subject_template.format("Python", "Daniel")
+
+    
